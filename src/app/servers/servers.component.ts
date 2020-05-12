87d306e7c044c6344a165a180a9be5dbb4e0b967
Waseem Khan Mohammad <mwkhan094@gmail.com>
2020-05-12 14:10:04 -0400

    Different way of using Selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,8 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // WORK: Selector: changing selector as attribute refer app.component.html
+  // selector:'[app-servers]',
+  // WORK: Selector: using it as a css property.
+  // selector:'.app-servers',
   templateUrl: './servers.component.html',
-  // WORK: Component Templates: 
+  // WORK: Component Templates: We can use inline HTML. If we have anything to show in HTML.
   // template: `
   // <app-server></app-server>
   // <app-server></app-server>
