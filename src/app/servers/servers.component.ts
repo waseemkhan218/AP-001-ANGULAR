074abdca3cbe91a088b88b9ac785d6777fc93c0d
Waseem Khan Mohammad <mwkhan094@gmail.com>
2020-05-12 10:04:15 -0400

    Component Templates

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,6 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
+  // WORK: Component Templates: 
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>
+  // `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
