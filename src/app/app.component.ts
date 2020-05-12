e91bed4bffe1af2ee1fd2742c093d7ba041dc916
Waseem Khan Mohammad <mwkhan094@gmail.com>
2020-05-12 13:53:18 -0400

    Inline styles in TS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,12 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // WORK: Inline Style in TS file.
+  // styles: [`
+  // h1 {
+  //   color:dodgerblue;
+  // }
+  // `]
 })
 export class AppComponent {
   title = 'my-first-app';
