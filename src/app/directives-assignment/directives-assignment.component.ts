1b6cd3d11bce1d3ea49df2017fcbba209ee2dfe3
Waseem Khan Mohammad <mwkhan094@gmail.com>
2020-05-13 19:57:51 -0400

    Directives assignment index example

--- src/app/directives-assignment/directives-assignment.component.ts
+++ src/app/directives-assignment/directives-assignment.component.ts
@@ -15,6 +15,7 @@ log = [];
 
   onToggle() {
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 }
