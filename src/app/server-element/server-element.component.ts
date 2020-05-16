406c0e44fb49f5d92719ec78c5d22af3dbe518c0
Waseem Khan Mohammad <mwkhan094@gmail.com>
2020-05-16 16:03:34 -0400

    Assigning an alias to custom properties

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -7,8 +7,14 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
   // WORK: Property binding and event binding: Passing element property to the app component.
-  // @Input() allow access to outside of the component.
-@Input() element: {type: string, name: string, content: string};
+  // @Input() allow access to outside of the component also called binding to custom properties.
+
+// @Input() element: {type: string, name: string, content: string};
+
+  //WORK: Assigning an alias to custom properties
+
+  @Input('SrvElement') element: {type: string, name: string, content: string};
+
   constructor() { }
 
   ngOnInit(): void {
