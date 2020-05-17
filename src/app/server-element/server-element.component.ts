fe24990448fb0182f4f8fc8c0847b208a2cb5df4
Waseem Khan Mohammad <mwkhan094@gmail.com>
2020-05-17 14:31:24 -0400

    View Encapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  // WORK: View Encapsulation: it's used for styles. We have 4 types in it.
+  // 1. Emulated is the default one. 2. None. 3. Native. 4. ShadowDom
+  encapsulation: ViewEncapsulation.None
 })
 export class ServerElementComponent implements OnInit {
   // WORK: Property binding and event binding: Passing element property to the app component or listening to app component.
