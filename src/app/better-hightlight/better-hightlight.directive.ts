1ed86c33fbd9cca83be33c5ebced7c8006b4dd72
Waseem Khan Mohammad <mwkhan094@gmail.com>
2020-05-25 11:32:47 -0400

    clean-up

--- src/app/better-hightlight/better-hightlight.directive.ts
+++ src/app/better-hightlight/better-hightlight.directive.ts
@@ -7,16 +7,16 @@ import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } f
 })
 export class BetterHightlightDirective implements OnInit {
 
- // WORK: HostBinding: We can the previous which is a better way of accessing depending on the use case.
- // But here, We are just trying to access the style from DOM. So, we can use this approach as well.
- // NOTE: In host binding: We can use the 'background-color' because DOM doesn't know the Dashes (-). 
- // Always use camelCase.
+  // WORK: HostBinding: We can the previous which is a better way of accessing depending on the use case.
+  // But here, We are just trying to access the style from DOM. So, we can use this approach as well.
+  // NOTE: In host binding: We can use the 'background-color' because DOM doesn't know the Dashes (-). 
+  // Always use camelCase.
   @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
- // WORK: Better Directive: Using renderer for better access the DOM.  
+  // WORK: Better Directive: Using renderer for better access the DOM.  
   constructor(private eleRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
- // WORK: Better Directive: Using setStyle method to set the background using elementRef.   
+    // WORK: Better Directive: Using setStyle method to set the background using elementRef.   
     // this.renderer.setStyle(this.eleRef.nativeElement, 'background-color', 'blue');
   }
 
@@ -32,9 +32,9 @@ export class BetterHightlightDirective implements OnInit {
   //   this.renderer.setStyle(this.eleRef.nativeElement, 'background-color', 'transparent');
   // }
 
-   // WORK: HostListener to host events: Used for listening official events. 
-   @HostListener('mouseenter') mouseover(eventData: Event) {
-     this.backgroundColor = 'blue';
+  // WORK: HostListener to host events: Used for listening official events. 
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.backgroundColor = 'blue';
   }
 
   // WORK: HostListener to host events: on mouseleave. 
